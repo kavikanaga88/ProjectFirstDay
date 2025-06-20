128bc9f3e5eb4021e9bcba5444c888907976fa63
Divino-24 <divinokeba1024@gmail.com>
2025-06-20 11:22:47 +0530

    Divino

--- src/test/java/org/project/ProjectFirstDay.java
+++ src/test/java/org/project/ProjectFirstDay.java
@@ -5,7 +5,9 @@ public class ProjectFirstDay {
 public static void main(String[] args) {
 	
 	System.out.println("project");
-	System.out.println("git develpos");
+	System.out.println("git develp");
+	System.out.println("Music is my life");
+	System.out.println("1st rank pradeep CJ");
 }
 
 }
